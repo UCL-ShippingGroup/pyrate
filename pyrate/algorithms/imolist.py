f49be217a666dee18ba57868cedb923dbeb06908
Sam Macbeth <sm1106@imperial.ac.uk>
2015-03-19 11:59:51 +0000

    docstrings for imo_list functions.

--- pyrate/algorithms/imolist.py
+++ pyrate/algorithms/imolist.py
@@ -10,20 +10,34 @@ def run(inp, out):
     create_imo_list(out['aisdb'])
 
 def create_imo_list(aisdb):
+    """Create the imo list table from mmsi, imo pairs in clean and dirty tables.
 
-    # insert data from clean db
+    This method collects the unique mmsi, imo pairs from a table, and the time
+    intervals over-which they have been seen in the data. These tuples are
+    then upserted into the imo_list table.
+
+    On the clean table pairs with no IMO number are also collected to get the
+    activity intervals of MMSI numbers. On the dirty table only messages
+    specifying an IMO are collected."""
+
     with aisdb.conn.cursor() as cur:
         start = time.time()
+
+        # collect existing set of mmsi, imo tuples in imo_list
         cur.execute("SELECT mmsi, imo FROM {}".format(aisdb.imolist.get_name()))
         existing_tuples = set(cur.fetchall())
         logging.info("Existing mmsi, imo pairs = %d (%fs)", len(existing_tuples), time.time()-start)
 
+        # query for mmsi, imo, interval tuples from clean db, and then upsert into
+        # imo_list table.
         logging.info("Getting mmsi, imo pairs from clean db")
         start = time.time()
         cur.execute("SELECT mmsi, imo, MIN(time), MAX(time) FROM {} GROUP BY mmsi, imo".format(aisdb.clean.get_name()))
         logging.info("Got new mmsi, imo pairs list (%fs)", time.time()-start)
         _upsert_imo_tuples(aisdb, cur, existing_tuples)
 
+        # query for mmsi, imo, interval tuples from dirty db, and then upsert into
+        # imo_list table.
         logging.info("Getting mmsi, imo pairs from dirty db")
         start = time.time()
         cur.execute("SELECT mmsi, imo, MIN(time), MAX(time) FROM {} WHERE message_id = 5 GROUP BY mmsi, imo".format(aisdb.dirty.get_name()))
@@ -33,6 +47,12 @@ def create_imo_list(aisdb):
         aisdb.conn.commit()
 
 def _upsert_imo_tuples(aisdb, result_cursor, existing_tuples):
+    """Inserts or updates rows in the imo_list table depending on the mmsi, imo
+    pair's presence in the table. result_cursor is a iterator of (mmsi, imo,
+    start, end) tuples. existing_tuples is a set of (mmsi, imo) pairs which
+    should be updated rather than inserted.
+    """
+
     with aisdb.conn.cursor() as insert_cur:
         start = time.time()
         insert_ctr = 0
@@ -50,6 +70,21 @@ def _upsert_imo_tuples(aisdb, result_cursor, existing_tuples):
         logging.info("Inserted %d new rows, updated %d rows (%f)", insert_ctr, update_ctr, time.time()-start)
 
 def filter_good_ships(aisdb):
+    """Generate a set of imo numbers and (mmsi, imo) validity intervals, for
+    ships which are deemed to be 'clean'. A clean ship is defined as one which:
+     * Has valid MMSI numbers associated with it.
+     * For each MMSI number, the period of time it is associated with this IMO
+    (via message number 5) overlaps with the period the MMSI number was in use.
+     * For each MMSI number, its usage period does not overlap with that of any
+    other of this ship's MMSI numbers.
+     * That none of these MMSI numbers have been used by another ship (i.e.
+    another IMO number is also associated with this MMSI)
+
+    Returns the tuple (valid_imos, imo_mmsi_intervals), where:
+     * valid_imos is a set of valid imo numbers
+     * imo_mmsi_intervals is a list of (mmsi, imo, start, end) tuples, describing
+     the validity intervals of each mmsi, imo pair.
+    """
 
     with aisdb.conn.cursor() as cur:
         cur.execute("SELECT distinct imo from {}".format(aisdb.imolist.get_name()))
