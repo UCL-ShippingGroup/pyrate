74fb26db0f978c4448dda16899277a286971a50c
Eoin O'Keeffe <eokeeffe@gmail.com>
2015-04-01 10:07:42 +0100

    Added code for detect_outliers

--- pyrate/utils.py
+++ pyrate/utils.py
@@ -1,3 +1,6 @@
+import Geodesic
+import pandas as pd
+import numpy
 
 def valid_mmsi(mmsi):
     """Checks if a given MMSI number is valid. Returns true if mmsi number is 9 digits long."""
@@ -55,7 +58,116 @@ def detect_outliers(msg_stream):
     ais_clean table. The list of messages should be ordered by timestamp in
     ascending order.
     """
-    return msg_stream, []
+    max_allow_speed_nm_hr=50
+    
+    
+    def get_dist(row):
+        #print row
+        try:
+            straight_line=Geodesic.WGS84.Inverse(row['lat'],row['lon'],\
+                row['next_lat'],row['next_lon'])
+            dist=straight_line['s12']/1000
+            return dist
+        except:
+            return float('inf')
+            
+            
+    df_orig=pd.DataFrame.from_records(msg_stream)
+    df_dist=pd.DataFrame.from_records(msg_stream)
+    
+    #calculate the required speed between data points
+    df_dist.sort('date_time',inplace=True)    
+    df_dist.reset_index(inplace=True,drop=True)
+    df_dist['next_lat']=df_dist['lat'].shift(periods=1)
+    df_dist['next_lon']=df_dist['lon'].shift(periods=1)
+    df_dist['dist']=df_dist.apply(get_dist,axis=1)
+    df_dist['elapsed_time']=df_dist.date_time.diff(1)
+    
+    
+    def get_speed(row):
+        if (row.elapsed_time==None)|(row.elapsed_time==0.0):
+            return 0.0
+        else:
+            return row['dist']/(row.elapsed_time/(60*60))
+        
+    #get speed between consecutive points
+    df_dist.elapsed_time=df_dist.elapsed_time.astype('timedelta64[s]')
+    df_dist['required_speed']=df_dist.apply(get_speed,axis=1)    
+    
+    df_dist['location_flag']=0
+    df_dist['location_flag'].ix[\
+            df_dist.required_speed*1.852>max_allow_speed_nm_hr]=1
+    df_dist['orig_loc_flag']=df_dist['location_flag'].values
+    #Identify the index which was the source for the speed of greater than 50
+    df_dist['start_flag_index']=0        
+    df_dist['start_flag_index'].ix[df_dist.location_flag.diff(-1)==-1]=1
+    #Identify if following messages were also outliers
+    #i.e. we need to get the speed from the source message not the outlier
+    #walk forward from each location flag until we find the datapoint that
+    #is acceptable
+    for start_indx in df_dist.ix[df_dist.start_flag_index==1].index:
+        for ii in range(start_indx+1,df_dist.shape[0]):
+            try:
+                straight_line=Geodesic.WGS84.Inverse(df_dist['lat'].ix[ii],\
+                    df_dist['lon'].ix[ii],\
+                    df_dist['lat'].ix[start_indx],\
+                    df_dist['lon'].ix[start_indx])
+                dist=straight_line['s12']/(1000*1.852)
+            except:
+                dist=float('inf')
+            if pd.isnull(dist):
+                dist=float('inf')
+            #df_dist['st_dist'].ix[ii]=dist
+            elapsed_time=(df_dist['date_time'].ix[ii]-\
+                df_dist['date_time'].ix[start_indx]).seconds
+            speed=dist/(numpy.max([1.0,elapsed_time])/(60.0*60))
+            if speed>max_allow_speed_nm_hr:
+                df_dist.location_flag.ix[ii]=1
+            else:
+                df_dist.location_flag.ix[ii]=0
+                break
+    
+     
+    #Now do speed flag
+    df_dist['speed_flag']=0
+    df_dist['speed_flag'].ix[pd.isnull(df_dist.sog)|(df_dist.sog>102.2)]=1
+    
+    # Now do course flag
+    df_dist['speed_flag']=0
+    df_dist['course_flag'].ix[pd.isnull(df_dist.course)|\
+        (df_dist.course>=360.0)|(df_dist.course<0.0)]=1
+    
+    # Now do heaidng flag
+    df_dist['heading_flag']=0
+    df_dist['heading_flag'].ix[pd.isnull(df_dist.heading)|\
+        (df_dist.heading>=360.0)|(df_dist.heading<0.0)]=1
+    
+    # Now do nav status flag
+    df_dist['nav_status_flag']=0
+    df_dist['nav_status_flag'].ix[\
+        df_dist.nav_status.isin([0,1,2,3,4,5,6,7,8,10,12,14])==False]=1
+        
+    #eta - don't need to check eta
+#    def check_eta(row):
+#        eta_chk=clean_eta(row,year=row.date_time)
+#        if eta_chk==None:
+#            return 1
+#        if eta_chk>row.date_time:
+#            return 0
+#        else:
+#            return 1
+#    df_dist['eta_flag']=df_dist.apply(check_eta,axis=1)
+#    df_dist['eta']=None
+#    df_dist['eta'].ix[df_dist.eta_flag==0]=df_dist.apply(clean_eta,axis=1)
+#    df_dist['eta']=pd.to_datetime(df_dist['eta'])
+    
+    #get indexes where the sum of the flags is zero
+    sum_flagged=df_dist[['speed_flag','speed_flag',\
+        'heading_flag','nav_status_flag','location_flag']].sum(axis=1)
+    unflag_indxs=df_dist.ix[sum_flagged==0].index
+    flag_indxs=df_dist.ix[sum_flagged>0].index
+    return [msg_stream[x] for x in unflag_indxs], \
+        [msg_stream[x] for x in flag_indxs]
 
 def interpolate_passages(msg_stream):
     """Interpolate far apart points in an ordered stream of messages.
