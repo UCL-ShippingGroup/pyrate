bc65c978563f0026d880d30d105a6401618ed6a9
Will Usher <w.usher@ucl.ac.uk>
2016-01-27 11:30:19 +0000

    Removed lines from travis scripts [ci skip]

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -3,10 +3,6 @@
 # .travis.yml. See http://docs.travis-ci.com/ for more details.
 # The behavior of the script is controlled by environment variabled defined
 # in the .travis.yml in the top level folder of the project.
-#
-# This script is taken from Scikit-Learn (http://scikit-learn.org/)
-#
-# THIS SCRIPT IS SUPPOSED TO BE AN EXAMPLE. MODIFY IT ACCORDING TO YOUR NEEDS!
 
 set -e
 
