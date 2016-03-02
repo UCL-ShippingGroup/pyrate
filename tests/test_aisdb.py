433b13741fb9fad20512d051f7b7564865be9c1d
Will Usher <w.usher@ucl.ac.uk>
2016-03-02 10:35:47 +0000

    Added setup functions for aisparser - input and output files

--- tests/test_aisdb.py
+++ tests/test_aisdb.py
@@ -1,16 +1,80 @@
 from pyrate.repositories.aisdb import AISdb
+from pyrate.repositories import file
+from pyrate.algorithms.aisparser import run
 import logging
+from pytest import fixture
 
-class TestAISdb:
-    """ Tests that the AIS database is correctly generated
+@fixture(scope='function')
+def setup_database(request):
+    """ Creates the required tables in the test_aisdb postgres database
+
+    Returns
+    -------
+    aisdb : pyrate.repositories.aisdb.AISdb
+        An instance of the AISdb class with tables created
+
     """
-    def test_load(self):
-        options = {'host': 'localhost',
-                   'db': 'test_aisdb',
-                   'user': 'postgres',
-                   'pass': ''}
-        aisdb = AISdb(options, readonly=False)
+    options = {'host': 'localhost',
+               'db': 'test_aisdb',
+               'user': 'postgres',
+               'pass': ''}
+    aisdb = AISdb(options, readonly=False)
+    with aisdb:
+        aisdb.create()
+    def teardown():
+        logging.debug("Tearing down database tables")
         with aisdb:
-            # logging.debug("Connections: {}".format(open_repo.conn.status)
-            aisdb.create()
-        assert aisdb.tables == ['']
+            cursor = aisdb.conn.cursor()
+            cursor.execute("drop schema public cascade;")
+    request.addfinalizer(teardown)
+    return aisdb
+
+@fixture(scope='function')
+def setup_input_csv_file():
+    """ Returns an open input file object
+
+    Returns
+    -------
+    pyrate.repositories.file.FileRepository
+
+    """
+    input_path = '/Users/will2/repository/exactEarth/tests/ais_import/data/ais'
+    input_options = {'path': input_path,
+                     'extensions': '.csv',
+                     'unzip': False,
+                     'recursive': False}
+    return file.load(input_options, readonly=True)
+
+@fixture(scope='function')
+def setup_log_csv_file():
+    """ Returns an open log file object
+
+    Returns
+    -------
+    pyrate.repositories.file.FileRepository
+
+    """
+    log_path = '/Users/will2/repository/exactEarth/tests/ais_import/data/bad'
+    log_options = {'path': log_path,
+                   'extensions': '.csv',
+                   'unzip': False,
+                   'recursive': False}
+    return file.load(log_options, readonly=False)
+
+class TestParsing():
+    """ Tests for parsing AIS data
+    """
+
+    def test_parser(self, setup_database, setup_input_csv_file, setup_log_csv_file):
+        """
+        """
+        input_object = setup_input_csv_file
+        bad_object = setup_log_csv_file
+
+        database = setup_database
+        inputs = {'aiscsv': input_object}
+        outputs = {'aisdb': database,
+                   'baddata': bad_object}
+        with database:
+            run(inp=inputs, out=outputs, dropindices=False, source=0)
+        assert 0 == 1
